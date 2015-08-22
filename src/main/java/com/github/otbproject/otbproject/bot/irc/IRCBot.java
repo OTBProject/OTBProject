f524e15ebdbc01a3895003d2ec4d9287fc3954e1
NthPortal <mail.vanpelt@gmail.com>
2015-08-22 01:58:47 -0400

    Fixed unchecked cast in constructor of IRCBot.

--- src/main/java/com/github/otbproject/otbproject/bot/irc/IRCBot.java
+++ src/main/java/com/github/otbproject/otbproject/bot/irc/IRCBot.java
@@ -13,9 +13,8 @@ import java.nio.charset.Charset;
 class IRCBot extends PircBotX {
     private final OutputRaw newOutputRaw;
 
-    @SuppressWarnings("unchecked")
     public IRCBot() {
-        super(new Configuration.Builder().setName(Configs.getAccount().getName()).setAutoNickChange(false).setCapEnabled(false).addListener(new IrcListener()).setServerHostname("irc.twitch.tv")
+        super(new Configuration.Builder<>().setName(Configs.getAccount().getName()).setAutoNickChange(false).setCapEnabled(false).addListener(new IrcListener()).setServerHostname("irc.twitch.tv")
                 .setServerPort(6667).setServerPassword(Configs.getAccount().getPasskey()).setEncoding(Charset.forName("UTF-8")).buildConfiguration());
         App.logger.info("Bot configuration built");
         newOutputRaw = new OutputRawImproved(this);
