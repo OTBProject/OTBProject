bfcec09a87a70cb59825089a2e522e551fa9a25d
NthPortal <nthportal@gmail.com>
2016-04-09 23:26:03 -0400

    Change entry point
    Change entry point and set logger.

--- src/main/scala/io/github/otbproject/otb/core/Core.scala
+++ src/main/scala/io/github/otbproject/otb/core/Core.scala
@@ -1,11 +1,11 @@
 package io.github.otbproject.otb.core
 
-import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.{LogManager, Logger}
 
 class Core {
-  private[otb] val logger: Logger = ??? // TODO: set logger somehow
+  private[otb] val logger: Logger = LogManager.getLogger()
 }
 
 object Core {
-  val core: Core = ??? // TODO: set Core somehow
+  val core: Core = new Core
 }
