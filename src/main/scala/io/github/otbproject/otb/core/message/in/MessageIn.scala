faba72cbd0cbdaa9046f954aebfa8836e12c966b
NthPortal <nthportal@gmail.com>
2016-04-09 23:31:38 -0400

    Tidy up MessageIn

--- src/main/scala/io/github/otbproject/otb/core/message/in/MessageIn.scala
+++ src/main/scala/io/github/otbproject/otb/core/message/in/MessageIn.scala
@@ -3,13 +3,8 @@ package io.github.otbproject.otb.core.message.in
 import io.github.otbproject.otb.core.ChannelUser
 
 final case class MessageIn(source: MessageSource, user: ChannelUser, text: String) {
-  private lazy val tokenizedText = text.split(" ").toStream.filterNot((s: String) => s.isEmpty).toList
-
   /**
-    * Returns the text of the message split on spaces, with duplicate
-    * spaces removed.
-    *
-    * @return a List of the text split on spaces
+    * The text of the message split on spaces, with duplicate spaces removed.
     */
-  def getTokenizedText: List[String] = tokenizedText
+  lazy val tokenizedText = text.split(" ").toStream.filterNot(_.isEmpty).toList
 }
